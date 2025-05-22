28de82a0f4ba3f7a16937f0e983afcc869cb4cbe
Matheus Lunardi <85383731+tavonha@users.noreply.github.com>
2025-05-22 20:42:05 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,28 @@
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open("app-cache").then(cache => {
-      return cache.addAll([
-        "./",
-        "./index.html",
-        "./popup.css",
-        "./popup.js",
-        "./icon.png"
-      ]);
-    })
+const CACHE_NAME = 'app-cache-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './popup.css',
+  './popup.js',
+  './icon.png'
+];
+
+self.addEventListener('install', event => {
+  console.log('[SW] Instalando...');
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('[SW] Cacheando recursos');
+        return cache.addAll(urlsToCache);
+      })
+      .catch(err => console.error('[SW] Falha ao cachear:', err))
   );
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(response => response || fetch(e.request))
+self.addEventListener('fetch', event => {
+  console.log(`[SW] Fetching: ${event.request.url}`);
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
   );
 });

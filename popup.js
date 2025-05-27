8d59524fcbda53062ac9fca482b6eb5b4461e823
Matheus Lunardi <85383731+tavonha@users.noreply.github.com>
2025-05-27 15:07:37 -0300

    Update popup.js

--- popup.js
+++ popup.js
@@ -293,7 +293,7 @@ document.addEventListener('DOMContentLoaded', function() {
             Dos(doomContainer, { wdosboxUrl: "https://js-dos.com/6.22/current/wdosbox.js" })
               .ready((fs, main) => {
                 fs.extract('assets/games/doom.zip').then(() => {
-                  main('DOOM.EXE');
+                  main(["DOOM.EXE"]); // <-- Corrigido aqui!
                 });
               });
         });
